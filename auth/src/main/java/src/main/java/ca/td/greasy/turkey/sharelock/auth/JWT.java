9a9ff03972c0369bb1e0c8b34f43c983f74836cc
Alex <alapena@mail.uoguelph.ca>
2019-09-21 14:32:16 -0400

    Auth PR

--- auth/src/main/java/src/main/java/ca/td/greasy/turkey/sharelock/auth/JWT.java
+++ auth/src/main/java/src/main/java/ca/td/greasy/turkey/sharelock/auth/JWT.java
@@ -2,7 +2,7 @@ package src.main.java.ca.td.greasy.turkey.sharelock.auth;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
-import java.security.Key;
+import java.security.Key;   
 import java.util.Base64;
 import java.util.Date;
 
@@ -12,18 +12,11 @@ public class JWT {
     private static final byte[] secretBytes = secret.getEncoded();
     private static final String base64SecretBytes = Base64.getEncoder().encodeToString(secretBytes);
 
-    private static String generateToken() {
-        Date exp = new Date(System.currentTimeMillis() + (1000 * 30));
-
-        String subject = "users/TzMUocMF4p";
-        String name = "Robert Token Man";
-        String scope = "self groups/admins";
-
+    private static String generateToken(Date expiry, String user, String lockID) {
         String token = Jwts.builder()
-                .setSubject(subject)
-                .setExpiration(exp)
-                .claim("name", name)
-                .claim("scope", scope)
+                .setExpiration(expiry)
+                .claim("user", user)
+                .claim("lockID", lockID)
                 .signWith(secret)
                 .compact();
 
@@ -35,15 +28,15 @@ public class JWT {
                 .setSigningKey(base64SecretBytes)
                 .parseClaimsJws(token).getBody();
         System.out.println("----------------------------");
-        System.out.println("Subject: " + claims.getSubject());
-        System.out.println("Name: " + claims.get("name"));
-        System.out.println("Scope: " + claims.get("scope"));
+        System.out.println("User: " + claims.get("user"));
+        System.out.println("lockID: " + claims.get("lockID"));
         System.out.println("Expiration: " + claims.getExpiration());
     }
 
     public static void main(String[] args) {
-        System.out.println(generateToken());
-        String token = generateToken();
-        verifyToken(token);
+        String token = generateToken(new Date(System.currentTimeMillis() + (1000 * 30)), "users/user1", "lockid123test");
+        String token2 = generateToken(new Date(System.currentTimeMillis() + (1000 * 30)), "users/user2", "lockid222test");
+        System.out.println(token);
+        verifyToken(token2);
     }
 }
