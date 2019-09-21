db377e13b0172217a53f1828f49e7d84f985a6f0
danruff5 <dckrauskopf@gmail.com>
2019-09-21 18:54:56 -0400

    Added in CrossOrigin

--- api/src/main/java/ca/td/greasy/turkey/sharelock/api/controller/KeyController.java
+++ api/src/main/java/ca/td/greasy/turkey/sharelock/api/controller/KeyController.java
@@ -12,6 +12,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class KeyController {
     
     @Autowired
--- api/src/main/java/ca/td/greasy/turkey/sharelock/api/controller/UserController.java
+++ api/src/main/java/ca/td/greasy/turkey/sharelock/api/controller/UserController.java
@@ -4,6 +4,7 @@ import ca.td.greasy.turkey.sharelock.api.model.User;
 import ca.td.greasy.turkey.sharelock.api.repository.UserRepository;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class UserController {
     
     @Autowired
